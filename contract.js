8626aa5207054f576942f56247c3aeb601debfc5
Stefan Thomas <justmoon@members.fsf.org>
2014-11-02 10:28:24 -0800

    [TASK] Cleaner example.

--- contract.js
+++ contract.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.get('/', function(req, res) {
-  console.log('got something!');  
+  res.set('Content-Type', 'text/plain');
+  res.send(fs.readFileSync('contract.js', 'utf-8')).end();
 });
 
 app.listen(process.env.PORT || 5000);
